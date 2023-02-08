8224324fcf4b9b2bc6aa2d86c71414953e6c8f11
rbrtknwls <r2knowle@uwaterloo.ca>
2023-02-08 02:54:14 -0500

    a

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -186,7 +186,7 @@ enter_forked_process(void* data1, unsigned long data2)
 {
     DEBUG(DB_THREADS,"Entering the forked process\n");
     (void)data2;
-    struct trapframe tf = (struct trapframe) data1;
+    struct trapframe tf = &((struct trapframe*) data1);
     DEBUG(DB_THREADS,"Trap has a epc of: %d | v0: %d\n", tf.tf_epc, tf.tf_v0);
 
     tf.tf_epc += 4;
