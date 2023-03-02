007330d6247178cd29c6b91990cfa04bca3ad2c7
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 18:42:06 -0500

    first

--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -165,7 +165,7 @@ lock_create(const char *name)
                 return NULL;
         }
 
-/*#ifdef OPT_A1
+#ifdef OPT_A1
 
         lock->lk_wchan = wchan_create(lock->lk_name);
         if (lock->lk_wchan == NULL) {
@@ -180,7 +180,7 @@ lock_create(const char *name)
         lock->lk_owner = NULL;
         lock->lk_held = false;
 
-#endif*/
+#endif
         
         return lock;
 }
