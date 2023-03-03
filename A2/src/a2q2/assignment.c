6cf018fdaa2d9c470eba3570c639f6d3eabf316a
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 23:10:47 -0500

    more

--- A2/src/a2q2/assignment.c
+++ A2/src/a2q2/assignment.c
@@ -14,7 +14,7 @@ consume_enter(struct resource *resource)
     if (isBelowRatio(resource->num_consumers + 1, resource->num_producers, resource->ratio))
         pthread_cond_wait(&resource->cond, &resource->mutex);
     resource->num_consumers += 1;
-     printf("CONSUME ENTER [p: %ld, c: %ld]\n", resource->num_consumers,  resource->num_producers);
+    printf("CONSUME ENTER [p: %ld, c: %ld]\n", resource->num_consumers,  resource->num_producers);
     pthread_cond_signal(&resource->cond);
     pthread_mutex_unlock(&resource->mutex);
 
@@ -24,9 +24,9 @@ void
 consume_exit(struct resource *resource)
 {
     // Consumer can always exit (increase ratio)
-    printf("CONSUME EXIT [c: %ld, p: %ld]\n", resource->num_consumers,  resource->num_producers);
     pthread_mutex_lock(&resource->mutex);
     resource->num_consumers -= 1;
+    printf("CONSUME EXIT [c: %ld, p: %ld]\n", resource->num_consumers,  resource->num_producers);
     pthread_cond_signal(&resource->cond);
     pthread_mutex_unlock(&resource->mutex);
 }
@@ -35,10 +35,10 @@ void
 produce_enter(struct resource *resource)
 {
     // Producers can always enter (increase ratio)
-    printf("PRODUCE ENTER [c: %ld, p: %ld]\n", resource->num_consumers,  resource->num_producers);
     // FILL ME IN
     pthread_mutex_lock(&resource->mutex);
     resource->num_producers += 1;
+    printf("PRODUCE ENTER [c: %ld, p: %ld]\n", resource->num_consumers,  resource->num_producers);
     pthread_cond_signal(&resource->cond);
     pthread_mutex_unlock(&resource->mutex);
 }
