7ff587f4393d68244c3cc13bf9ff709dc1495098
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 22:57:34 -0500

    poh

--- A2/src/a2q2/assignment.c
+++ A2/src/a2q2/assignment.c
@@ -11,12 +11,12 @@ consume_enter(struct resource *resource)
 {
     printf("CONSUME ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     // FILL ME IN
-    pthread_mutex_lock(&resource->mutex);
+    /*pthread_mutex_lock(&resource->mutex);
     if (isBelowRatio(resource))
         pthread_cond_wait(&resource->cond, &resource->mutex);
-    //resource->num_consumers += 1;
+    resource->num_consumers += 1;
     pthread_cond_signal(&resource->cond);
-    pthread_mutex_unlock(&resource->mutex);
+    pthread_mutex_unlock(&resource->mutex);*/
 
 }
 
@@ -25,10 +25,10 @@ consume_exit(struct resource *resource)
 {
     // Consumer can always exit (increase ratio)
     printf("CONSUME EXIT [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
-    pthread_mutex_lock(&resource->mutex);
-    //resource->num_consumers -= 1;
+    /*pthread_mutex_lock(&resource->mutex);
+    resource->num_consumers -= 1;
     pthread_cond_signal(&resource->cond);
-    pthread_mutex_unlock(&resource->mutex);
+    pthread_mutex_unlock(&resource->mutex);*/
 }
 
 void
@@ -37,22 +37,22 @@ produce_enter(struct resource *resource)
     // Producers can always enter (increase ratio)
     printf("PRODUCE ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     // FILL ME IN
-    pthread_mutex_lock(&resource->mutex);
-    //resource->num_producers += 1;
+    /*pthread_mutex_lock(&resource->mutex);
+    resource->num_producers += 1;
     pthread_cond_signal(&resource->cond);
-    pthread_mutex_unlock(&resource->mutex);
+    pthread_mutex_unlock(&resource->mutex);*/
 }
 
 void
 produce_exit(struct resource *resource)
 {
     printf("PRODUCE EXIT [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
-    pthread_mutex_lock(&resource->mutex);
+    /*pthread_mutex_lock(&resource->mutex);
 
     if (isBelowRatio(resource))
         pthread_cond_wait(&resource->cond, &resource->mutex);
-    //resource->num_consumers += 1;
-    pthread_mutex_unlock(&resource->mutex);
+    resource->num_consumers += 1;
+    pthread_mutex_unlock(&resource->mutex);*/
 
 }
 
