417150d4558c19d4acad8f03dcefc55560bf8c09
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 20:23:37 -0500

    OK

--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -207,7 +207,7 @@ lock_acquire(struct lock *lock)
 
 #ifdef OPT_A1
         KASSERT(lock != NULL);
-        KASSERT(lock->lk_held == false);
+
         spinlock_acquire(&lock->lk_spnlk);
         while (lock->lk_held) {
             wchan_lock(lock->lk_wchan);
@@ -316,10 +316,12 @@ cv_wait(struct cv *cv, struct lock *lock)
         KASSERT(lock != NULL);
         KASSERT(lock_do_i_hold(lock));
 
-//		wchan_lock(cv->cv_wchan);
-//		spinlock_release(&lock->lk_spnlk);
-//        wchan_sleep(cv->cv_wchan);
-//		spinlock_acquire(&lock->lk_spnlk);
+/*
+		wchan_lock(cv->cv_wchan);
+		spinlock_release(&lock->lk_spnlk);
+        wchan_sleep(cv->cv_wchan);
+		spinlock_acquire(&lock->lk_spnlk);
+		*/
 #endif
 
         // Write this
