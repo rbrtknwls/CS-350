3396751f237de920f52d8b6d700148b33ff50d28
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 22:45:36 -0500

    oj

--- A2/src/a2q2/assignment.c
+++ A2/src/a2q2/assignment.c
@@ -1,35 +1,57 @@
 #include "assignment.h"
 
+bool isBelowRatio(struct resource *resource) {
+    return resource->num_consumers > resource->num_producers * resource->ratio;
+}
+
 void
 consume_enter(struct resource *resource)
 {
     // FILL ME IN
-
+    pthread_mutex_lock(&resource->mutex);
+    if (isBelowRatio(resource))
+        pthread_cond_wait(&resource->cond, &resource->mutex);
     resource->num_consumers += 1;
+    pthread_cond_signal(&resource->cond);
+    pthread_mutex_unlock(&resource->mutex);
+
 }
 
 void
 consume_exit(struct resource *resource)
 {
-    // Consumer can always exit
+    // Consumer can always exit (increase ratio)
 
+    pthread_mutex_lock(&resource->mutex);
     resource->num_consumers -= 1;
+    if (!isBelowRatio(resource)) {
+        pthread_cond_signal(&resource->cond);
+    }
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_enter(struct resource *resource)
 {
-    // Producers can
+    // Producers can always enter (increase ratio)
     // FILL ME IN
+    pthread_mutex_lock(&resource->mutex);
     resource->num_producers += 1;
-
+    if (!isBelowRatio(resource)) {
+        pthread_cond_signal(&resource->cond);
+    }
+    pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_exit(struct resource *resource)
 {
-    // FILL ME IN
-    resource->num_consumers -= 1;
+
+    pthread_mutex_lock(&resource->mutex);
+    if (isBelowRatio(resource))
+        pthread_cond_wait(&resource->cond, &resource->mutex);
+    resource->num_consumers += 1;
+    pthread_mutex_unlock(&resource->mutex);
 
 }
 
