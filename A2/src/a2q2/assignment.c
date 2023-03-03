ca0f33e40b77b7916d980b5c65f2482ef4492bc5
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 22:54:50 -0500

    more

--- A2/src/a2q2/assignment.c
+++ A2/src/a2q2/assignment.c
@@ -9,10 +9,9 @@ bool isBelowRatio(struct resource *resource) {
 void
 consume_enter(struct resource *resource)
 {
-
+    printf("CONSUME ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     // FILL ME IN
     pthread_mutex_lock(&resource->mutex);
-    printf("CONSUME ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     if (isBelowRatio(resource))
         pthread_cond_wait(&resource->cond, &resource->mutex);
     //resource->num_consumers += 1;
@@ -25,13 +24,10 @@ void
 consume_exit(struct resource *resource)
 {
     // Consumer can always exit (increase ratio)
-
-    pthread_mutex_lock(&resource->mutex);
     printf("CONSUME EXIT [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
+    pthread_mutex_lock(&resource->mutex);
     //resource->num_consumers -= 1;
-    if (!isBelowRatio(resource)) {
-        pthread_cond_signal(&resource->cond);
-    }
+    pthread_cond_signal(&resource->cond);
     pthread_mutex_unlock(&resource->mutex);
 }
 
@@ -39,22 +35,20 @@ void
 produce_enter(struct resource *resource)
 {
     // Producers can always enter (increase ratio)
+    printf("PRODUCE ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     // FILL ME IN
     pthread_mutex_lock(&resource->mutex);
-    printf("PRODUCE ENTER [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
     //resource->num_producers += 1;
-    if (!isBelowRatio(resource)) {
-        pthread_cond_signal(&resource->cond);
-    }
+    pthread_cond_signal(&resource->cond);
     pthread_mutex_unlock(&resource->mutex);
 }
 
 void
 produce_exit(struct resource *resource)
 {
-
-    pthread_mutex_lock(&resource->mutex);
     printf("PRODUCE EXIT [p: %ld, c: %ld]", resource->num_consumers,  resource->num_producers);
+    pthread_mutex_lock(&resource->mutex);
+
     if (isBelowRatio(resource))
         pthread_cond_wait(&resource->cond, &resource->mutex);
     //resource->num_consumers += 1;
