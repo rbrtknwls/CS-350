3d4ba91a6750059dc574cc75a48d6ebdc4141441
rbrtknwls <r2knowle@uwaterloo.ca>
2023-03-02 20:47:19 -0500

    MORE

--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -317,7 +317,7 @@ cv_wait(struct cv *cv, struct lock *lock)
         KASSERT(lock_do_i_hold(lock));
 
 		wchan_lock(cv->cv_wchan);
-		panic("LOCK AND LOCK: %p | %p", lock->lk_spnlk, curthread);
+		panic("LOCK AND LOCK: %p | %p", &lock->lk_spnlk, curthread);
 		spinlock_release(&lock->lk_spnlk);
         wchan_sleep(cv->cv_wchan);
 		spinlock_acquire(&lock->lk_spnlk);
