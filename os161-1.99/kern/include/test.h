d2cf4239318b4908a082070a09efa5443af872fc
rbrtknwls <r2knowle@uwaterloo.ca>
2023-04-08 16:39:19 -0400

    more

--- os161-1.99/kern/include/test.h
+++ os161-1.99/kern/include/test.h
@@ -87,9 +87,9 @@ int nettest(int, char **);
 #ifdef OPT_A2
 int runprogram(int argc, char *argv[]);
 vaddr_t argcopy_out (vaddr_t *pointer, const char* str);
-char **args_alloc();
 void args_free(char **args);
 int argcopy_in(char **args, char **argv);
+char **args_alloc();
 #else
 int runprogram(char *progname);
 #endif
